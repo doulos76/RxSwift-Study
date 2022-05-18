b93040691a86020599c3fcbcd5f141a7b48a8eef
Minhoi Goo <27220138+doulos76@users.noreply.github.com>
2022-05-19 08:15:32 +0900

    update Filtering Operator

--- RxSwiftStudy/Playgrounds/Operators/FilteringOperator.playground/Contents.swift
+++ RxSwiftStudy/Playgrounds/Operators/FilteringOperator.playground/Contents.swift
@@ -60,4 +60,59 @@ Observable.of("😀", "😁", "🥹", "😅", "😎", "🤪", "🐶", "😝", "�
   .subscribe(onNext: { print($0) })
   .disposed(by: disposeBag)
 
+print("\n===============[ skip(until) ]===============\n")
+// skip(until:)
+// Returns the elements from the source observable sequence that are emitted after the other observable sequence produces an element.
 
+let guest = PublishSubject<String>()
+let openTime = PublishSubject<String>()
+
+guest
+  .skip(until: openTime)
+  .subscribe(onNext: { print($0) })
+  .disposed(by: disposeBag)
+
+guest.onNext("🤓")
+guest.onNext("😅")
+openTime.onNext("🔔 open")
+guest.onNext("😎")
+guest.onNext("😂")
+
+print("\n===============[ take ]===============\n")
+// Returns a specified number of contiguous elements from the start of an observable sequence.
+
+Observable.of("🥇", "🥈", "🥉", "🤓", "😎")
+  .take(3)
+  .subscribe(onNext: { print($0) })
+  .disposed(by: disposeBag)
+
+print("\n===============[ takeWhile ]===============\n")
+// Returns elements from an observable sequence as long as a specified condition is true.
+Observable.of("🥇", "🥈", "🥉", "🤓", "😎")
+  .take(while: { $0 != "🥉" })
+  .subscribe(onNext: { print($0) })
+  .disposed(by: disposeBag)
+
+print("\n===============[ takeUntil ]===============\n")
+// Returns the elements from the source observable sequence until the other observable sequence produces an element.
+
+let enrollment = PublishSubject<String>()
+let deadline = PublishSubject<String>()
+
+enrollment
+  .take(until: deadline)
+  .subscribe(onNext: { print($0) })
+  .disposed(by: disposeBag)
+
+enrollment.onNext("😎")
+enrollment.onNext("🤓")
+deadline.onNext("End")
+enrollment.onNext("🥹")
+
+print("\n===============[ distinctUntilChanged ]===============\n")
+// Returns an observable sequence that contains only distinct contiguous elements according to equality operator.
+
+Observable.of("저는", "저는", "앵무새", "앵무새", "앵무새", "입니다.", "저는", "저는", "저는", "앵무새", "앵무새", "일까요?", "일까요?")
+  .distinctUntilChanged()
+  .subscribe(onNext: { print($0) })
+  .disposed(by: disposeBag)
